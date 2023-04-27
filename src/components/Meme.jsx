e7f76070c31d1a29938f47a34c85f783760b5746
Romeo <romeo.moreaux35@gmail.com>
2023-04-27 19:16:21 +0200

    add data and fetch random index

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,20 +1,37 @@
+import memesData from "../memesData.js"
+
 function Meme() {
+
+    let url
+
+    function getMemeImage() {
+        
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+        console.log(url)
+    }
 
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
-                    type="text" 
-                    className="form--input"
+                    type="text"
                     placeholder="Top text"
+                    className="form--input"
                 />
                 <input 
                     type="text"
-                    placeholder="Bottom text" 
-                    className="form--input" 
+                    placeholder="Bottom text"
+                    className="form--input"
                 />
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
         </main>
     )
   }
